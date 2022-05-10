9e3c5c9ef98f340c7cd5e645fb0237e244eea6d0
Shreya <87652405+ShreyaSudheer@users.noreply.github.com>
2022-05-10 17:26:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
+song = "";
+
 function preload()
 {
+    song = loadSound('music.mp3');
 }
-
 function setup()
 {
-    canvas = createCanvas(280, 280);
+    canvas = createCanvas(400, 400);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
 }
 function draw()
 {
-
+    image(video, 0, 0, 600, 500);
+}
+function song_name()
+{
+    song.play();
+    song.rate(1);
+    song.setVolume(1);
 }
